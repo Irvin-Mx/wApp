51b572cc713e3cb77c9cc60c44c7cba6f67dc85b
Irvin Martinez <irvin0329@outlook.com>
2024-11-19 10:42:18 -0600

    Component cover started

---
+++ src/components/cover/Cover.jsx
@@ -0,0 +1,10 @@
+export default function Cover (){
+    return (
+        <section>
+            <h1>Marcos & Miriam</h1>
+            <h3>NOS CASAMOS</h3>
+            <h3>Save the date</h3>
+            <h3>01 ENE 2025</h3>
+        </section>
+    )
+}
